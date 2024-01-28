a44588cf0262fbca00e9eccf82b52917c300b21b
AbdullahAzza1 <abdullahazza141@gmail.com>
2024-01-28 20:33:56 +0100

    mod1

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,8 @@
 <header class="p-3">
+    <h1>ciao</h1>
     <img class="logo" src="{{ Vite::asset('resources/img/logo.png') }}" alt="Laravel Logo">
     <span>Some links: </span>
     @foreach ($links as $link)
-        <a href="{{ $link['href'] }}">{{ $link['text'] }}</a>
+    <a href="{{ $link['href'] }}">{{ $link['text'] }}</a>
     @endforeach
 </header>
